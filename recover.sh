5902d03fda895112ab05659a7bc25f09ab392967
Elian Doran <contact@eliandoran.me>
2022-05-14 16:20:25 +0300

    Allow the user to run the script directly

--- recover.sh
+++ recover.sh
@@ -1,33 +1,43 @@
 #!/usr/bin/env bash
 
-#
-# Create a temporary directory.
-#
+function buildTempDir {
+    dir=`mktemp -d`
 
-dir=`mktemp -d`
+    function cleanup {
+        rm -rf "$dir"
+    }
 
-function cleanup {
-    rm -rf "$dir"
+    if [ ! -e "$dir" ]; then
+        >&2 echo "Unable to create a temporary directory to store the XML files."
+        exit 1
+    fi
+
+    trap cleanup EXIT
 }
 
-if [ ! -e "$dir" ]; then
-    >&2 echo "Unable to create a temporary directory to store the XML files."
-    exit 1
-fi
+function askForExport {
+    #
+    # Ask the user to add the XML files.
+    #
+    echo "In KeeWeb, go to the file settings and select \"Save to...\", then select \"XML\"."
+    echo "Save the XML to the following temporary directory: $dir"
+    echo "Please don't save it anywhere else to avoid security issues. The XML files are not encrypted."
 
-trap cleanup EXIT
+    if command -v xdg-open &> /dev/null
+    then
+        xdg-open $dir &> /dev/null
+    fi
 
-#
-# Ask the user to add the XML files.
-#
-echo "In KeeWeb, go to the file settings and select \"Save to...\", then select \"XML\"."
-echo "Save the XML to the following temporary directory: $dir"
-echo "Please don't save it anywhere else to avoid security issues. The XML files are not encrypted."
+    echo "After placing all your exported XML files, press Enter to continue."
+    read -n1
+}
 
-if command -v xdg-open &> /dev/null
+function patchXml {
+    
+}
+
+if [ $# -eq 1 ]
 then
-    xdg-open $dir &> /dev/null
+    patchXml "$1"
+    exit 0
 fi
-
-echo "After placing all your exported XML files, press Enter to continue."
-read -n1
